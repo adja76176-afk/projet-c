1936ba67ff7ef8599f3211069da0c5775d3f7983
adja76176-afk <adja76176@gmail.com>
2026-02-11 23:24:51 +0000

    exoversionning

---
+++ exoversionning.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int x,n,somme=0,cpt=0;
+    float moy;
+    do{
+printf("saisir n entier");
+scanf("%d", &n);
+    }while(n<0);
+    for(int i=0;i<n;i++){
+        do{
+            puts("veuillez saisir un entier negatif");
+            scanf("%d", &x);
+        }while(x>0);
+        printf("le nombre que tu as saisi est %d",x);
+
+    }
+return 0;
+}
