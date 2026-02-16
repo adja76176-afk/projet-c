7384122198b208c28107aad41ebde93dd9d83176
adja76176-afk <adja76176@gmail.com>
2026-02-16 00:15:05 +0000

    resolve merge

--- exoversionning.c
+++ exoversionning.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 int main() {
     int x,n,somme=0,cpt=0;
@@ -22,23 +22,4 @@ scanf("%d", &n);
     printf("la moyenne des nombres pairs est %d ",moy);
 return 0;
 }
-=======
-#include <stdio.h>
-int main() {
-    int x,n,somme=0,cpt=0;
-    float moy;
-    do{
-printf("saisir n entier");
-scanf("%d", &n);
-    }while(n<0);
-    for(int i=0;i<n;i++){
-        do{
-            puts("veuillez saisir un entier negatif");
-            scanf("%d", &x);
-        }while(x>0);
-        printf("le nombre que tu as saisi est %d",x);
 
-    }
-return 0;
-}
->>>>>>> bf6528d53eab7ed7bd4019745d212841c1f6b06c
